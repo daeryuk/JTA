389fbb374d66a02b82b0867d196d3922173facf4
daeryuk <eofbrdlehdtjf@naver.com>
2024-08-31 20:32:43 +0900

    result페이지 구현

---
+++ static/result.js
@@ -0,0 +1,15 @@
+function createStars() {
+    const starsContainer = document.querySelector('.stars');
+    const starCount = 100;
+
+    for (let i = 0; i < starCount; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.animationDelay = `${Math.random() * 2}s`;
+        starsContainer.appendChild(star);
+    }
+}
+
+createStars();
